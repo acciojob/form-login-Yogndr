b7aa0bbf5ee671640dac8f416ea34a96ea5464c1
Yogndr <106022206+Yogndr@users.noreply.github.com>
2024-12-27 13:24:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
+let first=document.getElementsByTagName("input")[0];
+let last=document.getElementsByTagName("input")[1];
+
 function getFormvalue() {
+	alert(`${first.value} ${last.value}`);
     //Write your code here
 
 }
